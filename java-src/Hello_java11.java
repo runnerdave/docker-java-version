cd00e93f69632ac6d357f909a63fd85d3bb3f516
runnerdave <runnerdave@gmail.com>
2018-05-23 21:12:29 +1000

    finished docker example for java11

--- java-src/Hello_java11.java
+++ java-src/Hello_java11.java
@@ -1,6 +1,16 @@
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+
 public class Hello_java11 {
+    static boolean fourDigit(String name) {
+        return name.length() == 4;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Java 11 example: JEP 323: Local-Variable Syntax for Lambda Parameters ##TODO##");
+        System.out.println("Java 11 example: JEP 323: Local-Variable Syntax for Lambda Parameters");
+        List<String> names = Arrays.asList("Pedro", "Juan", "Maria");
+        System.out.println(names);
+        System.out.println(names.stream().filter((var name) -> fourDigit(name)).collect(Collectors.toList()));
     }
 }
